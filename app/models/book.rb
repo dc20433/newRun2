662c4a19bb6b4aa560be937c603bd39f51b931f0
dc20433 <96485755+dc20433@users.noreply.github.com>
2025-03-17 12:39:51 -0400

    gpyml (#4)

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,7 @@
 class Book < ApplicationRecord
     has_rich_text :content
+    has_many_attached :images do |attachable|
+        # attachable.variant :thumb, {resize_to_limit: [500, 500], convert: :webp, saver: { subsample_mode: "on", strip: true, interlace: true, quality: 80 }}
+        attachable.variant :thumb, {resize_to_limit: [500, 500], format: :webp}
+    end
 end
